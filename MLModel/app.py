1d3f15fecde51bd7ce89bc8c654e54ffbb5953d9
Ashwin-Ramesh2607 <ashwin.cuber26@gmail.com>
2020-06-20 19:23:47 +0530

    add od inferencing script

---
+++ MLModel/app.py
@@ -0,0 +1,80 @@
+import copy
+import time
+import argparse
+
+import cv2
+import numpy as np
+
+from mmdet.apis import inference_detector, init_detector, show_result_pyplot
+
+
+def prepare_person_data(detections, homography):
+
+    # Create a list to store data related to detections, required for social distancing checks
+    # detection_data is a list of tuples where each tuple is of the format
+    # (detection_id, transformed_feet_points_array)
+    detection_data = []
+    for idx, detection in enumerate(detections):
+        feet_point = ((detection[0] + detection[2]) / 2, detection[3])
+        feet_point = np.array(feet_point, dtype=np.float32)[np.newaxis, :]
+        transformed_feet_point = projection.transform_coords(feet_point, homography)
+
+        detection_data.append(
+            (idx, {
+                'feet_point': transformed_feet_point,
+                'bbox': detection}))
+
+    return detection_data
+
+
+def main():
+
+    # Build the model from a config and model file
+    model = init_detector(FLAGS.config_path, FLAGS.model_path, device=FLAGS.device)
+
+    # Calculate the Homography matrix for Perspective Transformation
+    homography, distance_threshold = projection.get_homography(
+        FLAGS.input_video_path, 1.0)
+
+    # Create Video Capture and Writer objects
+    video_capture = cv2.VideoCapture(FLAGS.input_video_path)
+    video_writer = cv2.VideoWriter(
+        FLAGS.output_video_path,
+        cv2.VideoWriter_fourcc(*'VP90'),
+        video_capture.get(cv2.CAP_PROP_FPS),
+        (1500, 1080))
+
+
+    while True:
+
+        ret, frame = video_capture.read()
+        if not ret or frame_count == 2000:
+            break
+
+        result = inference_detector(model, frame)[0]
+
+        # Filter the detections with a low confidence threshold
+        detections = []
+        for box in result:
+            if box[4] >= FLAGS.score_thresh:
+                detections.append(box[:4].astype(np.int16))
+
+        video_writer.write(output_frame)
+
+        logs = 'Frames processed: ' + str(frame_count)
+        print('\r' + logs, end='')
+        frame_count += 1
+
+    total_time = time.time() - start_time
+    time_per_frame = round((total_time / frame_count) * 1000, 2)
+    print(f'\nAverage Latency per frame: {time_per_frame} ms')
+
+    video_capture.release()
+    video_writer.release()
+
+
+if __name__ == '__main__':
+    FLAGS = get_params.parse_arguments()
+    main()
+
+
