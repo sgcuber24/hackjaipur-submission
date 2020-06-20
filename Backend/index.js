0556f6facd68a5f5f1c10c116f206994c7a61572
G Sriram <sgcuber24@gmail.com>
2020-06-20 15:13:23 +0530

    Added routes in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,11 +6,14 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(express.json());
 
+const crowdCountRouter = require('./routes/crowdCountRoute');
 
 app.get('/', function (req, res) {
   res.send(
     'Welcome to this API! Navigate to /api/v1/count to view the results!'
   );
 });
+
+app.use('/api/v1/count', crowdCountRouter);
 
 module.exports = app;
